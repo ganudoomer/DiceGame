99fdb763f0caa96160a0aa372651061259d7d471
ganudoomer <ganuiscool@gmail.com>
2022-01-24 20:39:59 +0530

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Sree on 24/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
